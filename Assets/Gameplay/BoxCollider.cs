1e53dbbd84a3f735bc14f54e0111bcac313351be
Andrew Hartley <78095251+w9075817@users.noreply.github.com>
2023-02-24 14:52:14 +0000

    ITWAORDDSDSFDSAGGSg

--- Assets/Gameplay/BoxCollider.cs
+++ Assets/Gameplay/BoxCollider.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class BoxCollider : MonoBehaviour
@@ -9,31 +10,40 @@ public class BoxCollider : MonoBehaviour
     public GameObject PublicVariablesHolder;
     private PublicVariables wallCountscript;
 
-    public bool wall = false;
-    public bool turret = false;
-    public char area;
+    public UnityEngine.UI.Button turretButton;
+    public UnityEngine.UI.Button wallButton; 
 
+    static bool wall = false;
+    static bool turret = false;
+
+    static int limitWall = 20;
+    static int limitTurret = 4;
+
+    static int n_walls = 0;
+    static int n_turret = 0;
 
     void OnMouseDown()
     {
-        if (wall)
+        if (wall && n_walls <= limitWall)
         {
+            Debug.Log("Wall added");
             Instantiate(WallClass, transform.position, transform.rotation);
             Destroy(gameObject);
-
+            n_walls += 1;
             wallCountscript.wallCount += 1;
 
         }
-        else if (turret)
+        else if (turret && n_turret <= limitTurret)
         {
+            Debug.Log("Turret added");
             Instantiate(TurretClass, transform.position, transform.rotation);
             Destroy(gameObject);
-
+            n_turret += 1;
             wallCountscript.turretCount += 1;
         }
         else
         {
-            return;
+            Debug.Log("nothing");
         }
     }
 
@@ -41,34 +51,43 @@ public class BoxCollider : MonoBehaviour
     {
         wallCountscript = PublicVariablesHolder.GetComponent<PublicVariables>();
         
-        if(transform.position.x < 0)
-        {
-            area = 'a';
-        }
-        else if(transform.position.x > 0)
-        {
-            area = 'b';
-        }
+        UnityEngine.UI.Button turretButtonScr = turretButton.GetComponent<UnityEngine.UI.Button>();
+        turretButtonScr.onClick.AddListener(ChangeToTurret);
+        
+        UnityEngine.UI.Button wallButtonScr = wallButton.GetComponent<UnityEngine.UI.Button>();
+        wallButtonScr.onClick.AddListener(ChangeToWall);
+
     }
 
-    void ChangeToTurret()
+    public void ChangeToTurret()
     {
+        Debug.Log("Changed turret");
         turret = true;
         wall = false;
     }
 
+    public void ChangeToWall()
+    {
+        Debug.Log("Changed wall");
+        turret = false;
+        wall = true;
+    }
+
     void Update()
     {
-        if (Input.GetKey("a"))
-        {
-            turret = false;
-            wall = true;
-        }
+        
+      
+            
+        //if (Input.GetKey("a"))
+        //{
+        //    turret = false;
+        //    wall = true;
+        //}
 
-        if (Input.GetKey("d"))
-        {
-            turret = true;
-            wall = false;
-        }
+        //if (Input.GetKey("d"))
+        //{
+        //    turret = true;
+        //    wall = false;
+        //}
     }
 }
