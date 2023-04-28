b86faf7afc6db2b2e0e6355e1a47e36990803a89
Andrew Hartley <78095251+w9075817@users.noreply.github.com>
2023-04-28 14:33:12 +0100

    push

--- Assets/New GamePlay/Scripts/EnemyScript.cs
+++ Assets/New GamePlay/Scripts/EnemyScript.cs
@@ -5,11 +5,16 @@ public class EnemyScript : MonoBehaviour
     GameObject Target;
     public GameObject destroyEffect;
     public GameObject damageEffect;
-    
+    public Material _machineLearningMAT;
 
     BaseScript baseScript;
     FlockingManager flockingManager;
     public float health = 4f;
+
+    public float Red = 0.0f;
+    public float Green = 0.0f;
+    public float Blue = 0.0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +22,33 @@ public class EnemyScript : MonoBehaviour
         Target = GameObject.FindGameObjectWithTag("Base");
         baseScript = Target.GetComponent<BaseScript>();
         flockingManager = gameObject.GetComponent<FlockingManager>();
+
+
+
+        //_machineLearningMAT.SetColor("MachineLearningMAT", new Color(Red,Green,Blue));
+        //GetComponent<Renderer>().material.SetColor("MachineLearningMAT", new Color(Red,Green,Blue));
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+        var enemyRenderer = this.GetComponent<Renderer>();
+
+        enemyRenderer.material.color = newColor;
+
+
+        //Red = Random.Range(0.0f, 1.0f);
+        //Green = Random.Range(0.0f, 1.0f);
+        //Blue = Random.Range(0.0f, 1.0f);
+        //var col = new Color(Red, Green, Blue, 0.5f);
+        //GetComponent<Renderer>().material.color = col;
+
+
+
         float distanceToTarget = (Target.transform.position - transform.position).magnitude;
         if (distanceToTarget <= 5)
         {
